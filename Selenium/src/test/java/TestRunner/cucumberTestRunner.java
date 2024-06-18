fc077983e560224aeaeeef432032ea81dea489d8
DELL <DELL@DESKTOP-LF9C4GT>
2024-06-18 16:42:00 +0530

    POM

--- Selenium/src/test/java/TestRunner/cucumberTestRunner.java
+++ Selenium/src/test/java/TestRunner/cucumberTestRunner.java
@@ -3,7 +3,7 @@ package TestRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "@Test", features = { "src/test/resources/Feature" }, glue = { "StepDefinations" }, plugin = {
+@CucumberOptions(tags = "", features = { "src/test/resources/Feature" }, glue = { "StepDefinations" }, plugin = {
 		"pretty", "html:target/htmlreport.html" })
 public class cucumberTestRunner extends AbstractTestNGCucumberTests {
 
--- Selenium/src/test/resources/Feature/login.feature
+++ Selenium/src/test/resources/Feature/login.feature
@@ -1,8 +1,12 @@
 Feature: Login page Automation
-  @Test
+
   Scenario: Check login is successful with valid creds
   Given User is on login page
-  When User enters valid username and password
+  When User enters valid "<username>" and "<password>"
   And Clicks on login Button
   Then User is navigated to home page
   And Close the browser
+  
+  Examples:
+  |username|password|
+  |standard_user|secret_sauce|
